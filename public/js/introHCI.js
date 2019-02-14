a3522ce7d9733b6bcbdc8e6b2ada386bb89bf960
Erich McMillan <emcmilla@ucsd.edu>
2019-02-14 10:45:04 -0800

    What I got to

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,6 +25,8 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	$.get("/project":projectID)
 
 	console.log("User clicked on project " + idNumber);
 }
+function callBack(){}
